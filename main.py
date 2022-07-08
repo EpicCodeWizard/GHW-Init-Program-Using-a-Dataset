b8141ad7ceff045a4f591ed91f42df3791cc3248
EpicCodeWizard <83260377+EpicCodeWizard@users.noreply.github.com>
2022-07-07 22:11:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import cohere
 co = cohere.Client("2H7QHvLLgO1HK4kzz6w7EBLpoEHVJnYOTH0UbGF5")
 dataset = [Example(x.split("\t")[1], x.split("\t")[0]) for x in open("preds.txt", "r").read().split("\n")]
 classifications = co.classify(model="medium", taskDescription="The following is a sentiment classifier regarding an input the user gives.", outputIndicator="this is:", inputs=input("Input something to be classified as positive or negative. "), examples=dataset).classifications
+for class in classifications:
+  if class.prediction == "positive":
+    print("positive")
+  else:
+    print("negative")
